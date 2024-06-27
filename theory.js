1ca4f1d46b2ce456a1619ab60452e41847b2ab4f
Ambika <ambikapatel125@gmail.com>
2024-06-27 12:48:49 +0530

    all files are tracked

---
+++ theory.js
@@ -0,0 +1,16 @@
+/*
+
+! <h3>What is GitHub?</h3>
+
+<li>Defination1: GitHub is a cloud-based hosting service that lets us manage Git repositories.</li>
+<li>The key term here is cloud based hosting service, and then it manages our port
+gates repository.</li>
+<li>Defination2: GitHub is a hosted service that provides access to a central repositary for coding
+projects.It also offers features such as issue tracking, milestone management, and wikis.</li>
+<li>GitHub is a cloud based provider where we can keep all our projects that has been initialized using
+Gits.</li>
+
+! <h3>What is Git?</h3>
+
+<li>Git is a version control system.</li>
+*/
